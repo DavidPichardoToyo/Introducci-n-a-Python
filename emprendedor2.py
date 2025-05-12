2daeaace1f3950242390150bfb8a2f4ea944b838
DavidPichardoToyo <dpichardot31@gmail.com>
2025-05-12 17:04:16 +0000

    avance

--- emprendedor2.py
+++ emprendedor2.py
@@ -4,7 +4,7 @@ import math as m
 print('Programa para calcular las utilidades de un proyecto')
 
 P = float(input("Ingrese precio de suscripción:"))
-U = int(input("Ingrese el número de usuarios:"))
+U = int(input("Ingrese el número de usuarios normales:"))
 UP = int(input("Ingrese el número de usuarios premium:"))
 GT = float(input("Ingrese los gastos:"))
 
