5f8a191888b44efff5e0032eacf29fe2dc68afd8
DavidPichardoToyo <dpichardot31@gmail.com>
2025-05-12 14:37:48 +0000

    Listo archivo escape

--- escape.py
+++ escape.py
@@ -2,11 +2,16 @@ import math
 
 import math as m
 
-r = float(input("Ingrese el radio en metros:"))
-g = float(input("Ingrese la constante g:"))
+print('Programa para calcular la velocidad de escape del planeta tierra')
+print('Tomar en cuenta que el valor de radio del planeta tierra es = 6371 km.') 
+print('Valor de la constante de gravedad es = 9.8 m/s2') 
 
-V = m.sqrt(2* r * g)
+r1 = float(input("Ingrese el radio del planeta en kilometros:"))
+g = float(input("Ingrese la constante gravedad(g):"))
 
-print(f'La velocidad de escape es: {V:.2f}')
+r = r1 * 1000
+v = m.sqrt(2* r * g)
 
-637
+print(f'La velocidad de escape del planeta tierra es: {v:.2f}m/s')
+
+
