8dd235a88671d2a47a0afe587478aadea37372b7
DavidPichardoToyo <dpichardot31@gmail.com>
2025-05-12 03:05:46 +0000

    avance

--- escape.py
+++ escape.py
@@ -1,3 +1,12 @@
 import math
 
 import math as m
+
+r = float(input("Ingrese el radio en metros:"))
+g = float(input("Ingrese la constante g:"))
+
+V = m.sqrt(2* r * g)
+
+print(f'La velocidad de escape es: {V:.2f}')
+
+637
